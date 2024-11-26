45492f5ee60f845fe49fe207faeb9aee298b320c
achraf ydir <achraf.AIT-DAOUD@etu.univ-amu.fr>
2024-11-26 16:48:19 +0100

    adding java files

---
+++ src/main/java/com/mycompany/maintaskmanager/MainTaskManager.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maintaskmanager;
+
+import java.util.*;
+import java.awt.event.*;
+import java.awt.*;
+
+/**
+ *
+ * @author aitda
+ */
+public class MainTaskManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new FirstWindow().setVisible(true);
+    }
+}
